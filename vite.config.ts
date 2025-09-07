0dcdf704fedb6a503a496a3fcf229b965710bee3
easypie65 <easypie65@gmail.com>
2025-09-07 21:35:52 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 깃허브 리포명이 'reponame'라면 base는 '/reponame/'
 export default defineConfig({
   plugins: [react()],
-  base: '/topgun/'
+  base: '/topgun/'   
 })
